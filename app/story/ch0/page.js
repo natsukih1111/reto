28cd552873fff31bdc171a598d280a9aedd99e10
natsukih1111 <natsuki.h1111@gmail.com>
2025-12-17 13:01:56 +0900

    update submit page

--- app/story/ch0/page.js
+++
@@ -1,8 +0,0 @@
-// file: app/story/ch0/page.js
-'use client';
-
-import StoryPlayer from '@/components/StoryPlayer';
-
-export default function StoryCh0Page() {
-  return <StoryPlayer chapterId="ch0" />;
-}
